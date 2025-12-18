652a91d57a865308031cabf4b7d236801c77a991
Tajun <tajunnaseera006@gmail.com>
2025-12-18 08:23:10 +0530

    Problem Solved

--- LEETCODE/Problem4.java
+++ LEETCODE/Problem4.java
@@ -1,5 +1,18 @@
-package LEETCODE;
-
-public class Problem4 {
-    
-}
+ class Solution {
+    public String removeOuterParentheses(String s) {
+        int cnt =0;
+    StringBuilder sb = new StringBuilder();
+       for(int i=0;i<s.length();i++){
+            if(s.charAt(i)==')'){
+                cnt --;
+            }
+            if(cnt!=0){
+                sb.append(s.charAt(i));
+            }
+            if(s.charAt(i)=='('){
+                cnt++;
+            }
+            }
+            return(sb.toString());
+        } 
+  }
--- LEETCODE/Problem5.java
+++ LEETCODE/Problem5.java
@@ -1,5 +1,13 @@
-package LEETCODE;
-
-public class Problem5 {
-    
+ class Solution {
+    public String reverseWords(String s) {
+        StringBuilder sb = new StringBuilder();
+        String[]str =s.trim().split("\\s+");
+       for(int i=str.length-1;i>=0;i--){
+            sb.append(str[i]);
+            if(i!=0){
+            sb.append( " " );
+            }
+        }
+        return sb.toString();
+     } 
 }
