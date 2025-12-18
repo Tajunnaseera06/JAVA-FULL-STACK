0a9fe796efa2c465f2f227e9182b828bbe87332f
Tajun <tajunnaseera006@gmail.com>
2025-12-18 13:12:27 +0530

    Problems Solved

---
+++ LEETCODE/Problem6.java
@@ -0,0 +1,22 @@
+ class Solution {
+    public int[] sortArrayByParity(int[] nums) {
+        int st=0,end=nums.length-1;
+        while (st<end)
+        {
+            if(nums[st]%2==0){
+                st++;
+            }
+            else if(nums[end]%2!=0){
+              end--;
+            }
+           else{
+            int t =nums[st];
+            nums[st]=nums[end];
+            nums[end]=t;
+            st++;
+            end--; 
+           }
+        }
+        return nums;
+    }
+}
