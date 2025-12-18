7a78cd839d0c43c3aeb2b8bc7d671b7cbc1ca4aa
Tajun <tajunnaseera006@gmail.com>
2025-12-18 07:56:31 +0530

    Problems Solved

--- DAY4/Transpose.java
+++ DAY4/Transpose.java
@@ -1,27 +1,27 @@
- public class Transpose {
+public class Transpose {
     public static void main(String[] args) {
-        int a [][]  ={
-            {1,2,3},
-            {4,5,6},
-            {7,8,9}
+        int a[][] = {
+                { 1, 2, 3 },
+                { 4, 5, 6 },
+                { 7, 8, 9 }
         };
-        for(int i=0;i<a.length;i++){
-            for( int j=i+1;j<a.length;j++){
-                
-                int temp=a[i][j];
-            a[i][j]=a[j][i];
-            a[j][i]=temp;
-           
+        for (int i = 0; i < a.length; i++) {
+            for (int j = i + 1; j < a.length; j++) {
+
+                int temp = a[i][j];
+                a[i][j] = a[j][i];
+                a[j][i] = temp;
+
             }
 
         }
-        for(int i=0;i<a.length;i++){
-            for( int j=0;j<a[i].length;j++){
-        System.out.print(a[i][j]+" ");
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a[i].length; j++) {
+                System.out.print(a[i][j] + " ");
             }
             System.out.println();
-                 
+
         }
-         
-          }
+
+    }
 }
